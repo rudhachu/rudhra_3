4fcd3116d878a125a99caaac107172522d0ac46a
rudhachu <120007671+rudhachu@users.noreply.github.com>
2024-09-05 11:40:41 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -1,27 +1,41 @@
- const { Rudhra, getUrl, igdl, isIgUrl,mode } = require("../lib/");
-Rudhra(
-  {
-    pattern: "insta ?(.*)",
+const { Rudhra, mode, getJson } = require("../lib");
+
+Rudhra({
+    pattern: 'insta ?(.*)', 
     fromMe: mode,
-    desc: "To download instagram media",
-    type: "user",
-  },
-  async (message, match) => {
+    desc: 'Send all media from Instagram URL.',
+    type: 'downloader'
+}, async (message, match) => {
+    
+    
     match = match || message.reply_message.text;
-    if (!match) return await message.sendMessage(message.jid, "Give me a link");
-    const url = getUrl(match.trim())[0];
-    try {
-      const data = await igdl(getUrl(match.trim())[0]);
-      if (data.length == 0)
-        return await message.sendMessage(
-          message.jid,
-          "No media found on the link"
-        );
-      data.forEach(async (url) => {
-        await message.sendFile(url);
-      });
-    } catch (e) {
-      await message.sendMessage(message.jid, "Error: " + e);
+
+   
+    if (!match) {
+        return await message.reply('Please provide an Instagram URL.');
     }
-  }
-);
+
+   
+    const instaUrl = match.trim();
+
+    try {
+        
+        const mediaUrl = `https://api.eypz.c0m.in/aio?url=${encodeURIComponent(instaUrl)}`;
+        
+        const mediaData = await getJson(mediaUrl);
+
+        
+        if (!mediaData || !mediaData.medias || mediaData.medias.length === 0) {
+            return await message.reply('No media found for the provided URL.');
+        }
+
+        
+        for (const media of mediaData.medias) {
+            await message.sendFromUrl(media.url);
+        }
+
+    } catch (error) {
+        console.error('Error fetching media:', error);
+        await message.reply('An error occurred while fetching media.');
+    }
+});
